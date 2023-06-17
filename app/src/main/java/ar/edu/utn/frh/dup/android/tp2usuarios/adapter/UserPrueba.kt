8f80bc6b794f06776aeaa825ed39ef85ac4828e0
MartinMirabelli <mirabellielec@gmail.com>
2023-06-17 15:43:05 -0300

    commit inicial

--- app/src/main/java/ar/edu/utn/frh/dup/android/tp2usuarios/adapter/UserPrueba.kt
+++ app/src/main/java/ar/edu/utn/frh/dup/android/tp2usuarios/adapter/UserPrueba.kt
@@ -6,11 +6,11 @@ class UserPrueba {
     companion object{
         val datosHarcodeados = listOf<Usuarios>(
             Usuarios(name = "Martin Mirabelli" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
-            Usuarios(name = "Martin Mirabelli" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
-            Usuarios(name = "Martin Mirabelli" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
-            Usuarios(name = "Martin Mirabelli" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
-            Usuarios(name = "Martin Mirabelli" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
-            Usuarios(name = "Martin Mirabelli" , city = "Buenos Aires" , country = "Argentina" , picture = "url")
+            Usuarios(name = "Juan Perez" , city = "Cordoba" , country = "Argentina" , picture = "url"),
+            Usuarios(name = "Mercedes Palacio" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
+            Usuarios(name = "Noemi de Napoli" , city = "Buenos Aires" , country = "Argentina" , picture = "url"),
+            Usuarios(name = "John Lennon" , city = "Liverpool" , country = "Reino Unido" , picture = "url"),
+            Usuarios(name = "Robert De Niro" , city = "Nueva York" , country = "Estados Unidos" , picture = "url")
         )
     }
 }
